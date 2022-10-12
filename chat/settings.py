8dc4a6568a379be73fe199da3af9e633621fe34b
sampat <sampat4123@gmail.com>
2022-10-12 21:13:16 +0530

    new auth screen

--- chat/settings.py
+++ chat/settings.py
@@ -21,8 +21,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('S_KEY')
-
+# SECRET_KEY = os.environ.get('S_KEY')
+SECRET_KEY = 'django-insecure-8f$*t_cl=zli*-r)@a8k5&m%28mt=^8fhipb&@xo5hwn#&35zq'
 # os.environ.setdefault("DJANGO_SETTINGS_MODULE", "chat.settings.local")
 
 
